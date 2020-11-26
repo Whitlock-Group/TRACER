a98391c6e7ada91a09c27f5b56c89fc9097fc8da
JacopoPaglia <61981085+JacopoPaglia@users.noreply.github.com>
2020-11-26 15:07:34 +0100

    Add files via upload
    Cleaner script

--- Navigate_Atlas.py
+++ Navigate_Atlas.py
@@ -5,7 +5,6 @@ Created on Thu Nov 26 09:33:18 2020
 @author: jacopop
 """
 
-
 from __future__ import print_function
 
 # First run the nex line in the console
@@ -57,90 +56,27 @@ def readlabel( file ):
     output_colors = np.array(output_colors)  # Use numpy array for  in the label file                  
     return [output_index, output_names, output_colors]
 
-# Classes to scroll the atlas slices (coronal, sagittal and horizontal)
-class IndexTracker_c(object):
-    def __init__(self, ax, X, pixdim):
+# Classes to scroll the atlas slices (coronal, sagittal and horizontal)    
+class IndexTracker(object):
+    def __init__(self, ax, X, pixdim, p):
         self.ax = ax
+        self.plane = p.lower()
         ax.set_title('Atlas viewer')
         print('use scroll wheel to navigate the atlas \n')
 
         self.X = X
-        if len(self.X.shape) == 3:
+        if self.plane == 'c':
             rows, self.slices, cols = X.shape
             self.ind = 540
             self.im = ax.imshow(self.X[:, self.ind, :].T, origin="lower", extent=[0, 512*pixdim, 0, 512*pixdim], cmap='gray')
-        elif len(self.X.shape) == 4:
-            rows, self.slices, cols, color = X.shape
-            self.ind = 653              
-            self.im = ax.imshow(self.X[:, self.ind, :].transpose((1,0,2)), origin="lower",alpha=0.5)
-        self.update()
-
-    def onscroll(self, event):
-        #print("%s %s" % (event.button, event.step))
-        if event.button == 'up':
-            self.ind = (self.ind + 1) % self.slices
-        else:
-            self.ind = (self.ind - 1) % self.slices
-        self.update()
-
-    def update(self):
-        if len(self.X.shape) == 3:
-            self.im.set_data(self.X[:, self.ind, :].T)
-        elif len(self.X.shape) == 4:             
-            self.im.set_data(self.X[:, self.ind, :].transpose((1,0,2)))
-        #ndimage.rotate(img, 45, reshape=False)
-        ax.set_ylabel('slice %d' % self.ind)
-        self.im.axes.figure.canvas.draw()  
-        
-class IndexTracker_s(object):
-    def __init__(self, ax, X, pixdim):
-        self.ax = ax
-        ax.set_title('Atlas viewer')
-        print('use scroll wheel to navigate the atlas \n')
-
-        self.X = X
-        if len(self.X.shape) == 3:
+        elif self.plane == 's':
             self.slices, rows, cols = X.shape
             self.ind = 246                
-            self.im = ax.imshow(self.X[self.ind, :, :].T, origin="lower", extent=[0 ,1024*pixdim, 0, 512*pixdim], cmap='gray')
-        elif len(self.X.shape) == 4:                 
-            self.slices, rows, cols, color = X.shape
-            self.ind = 246                
-            self.im = ax.imshow(self.X[self.ind, :, :].transpose((1,0,2)), origin="lower",alpha=0.5)
-        self.update()
-
-    def onscroll(self, event):
-        #print("%s %s" % (event.button, event.step))
-        if event.button == 'up':
-            self.ind = (self.ind + 1) % self.slices
-        else:
-            self.ind = (self.ind - 1) % self.slices
-        self.update()
-
-    def update(self):
-        if len(self.X.shape) == 3:
-            self.im.set_data(self.X[self.ind, :, :].T)
-        elif len(self.X.shape) == 4:             
-            self.im.set_data(self.X[self.ind, :, :].transpose((1,0,2)))
-        #ndimage.rotate(img, 45, reshape=False)
-        ax.set_ylabel('slice %d' % self.ind)
-        self.im.axes.figure.canvas.draw()               
-        
-class IndexTracker_h(object):
-    def __init__(self, ax, X, pixdim):
-        self.ax = ax
-        ax.set_title('Atlas viewer')
-        print('use scroll wheel to navigate the atlas \n')
-
-        self.X = X
-        if len(self.X.shape) == 3:
+            self.im = ax.imshow(self.X[self.ind, :, :].T, origin="lower", extent=[0 ,1024*pixdim, 0, 512*pixdim], cmap='gray')            
+        elif self.plane == 'h':  
             rows, cols, self.slices = X.shape
             self.ind = 440       
-            self.im = ax.imshow(self.X[:, :, self.ind].T, origin="lower", extent=[0, 512*pixdim, 0, 1024*pixdim], cmap='gray')
-        elif len(self.X.shape) == 4:                 
-            rows, cols, self.slices, color = X.shape
-            self.ind = 440         
-            self.im = ax.imshow(self.X[:, :, self.ind].transpose((1,0,2)), origin="lower",alpha=0.5)
+            self.im = ax.imshow(self.X[:, :, self.ind].T, origin="lower", extent=[0, 512*pixdim, 0, 1024*pixdim], cmap='gray')            
         self.update()
 
     def onscroll(self, event):
@@ -149,43 +85,47 @@ class IndexTracker_h(object):
             self.ind = (self.ind + 1) % self.slices
         else:
             self.ind = (self.ind - 1) % self.slices
-        self.update()        
-        
+        self.update()
+
     def update(self):
-        if len(self.X.shape) == 3:
+        if self.plane == 'c':
+            self.im.set_data(self.X[:, self.ind, :].T)
+        elif self.plane == 's':
+            self.im.set_data(self.X[self.ind, :, :].T)
+        elif self.plane == 'h':
             self.im.set_data(self.X[:, :, self.ind].T)
-        elif len(self.X.shape) == 4: 
-            self.im.set_data(self.X[:, :, self.ind].transpose((1,0,2)))
-        #ndimage.rotate(img, 45, reshape=False)
         ax.set_ylabel('slice %d' % self.ind)
-        self.im.axes.figure.canvas.draw()   
+        self.im.axes.figure.canvas.draw()          
+            
         
-class IndexTracker_cg(object):
-    def __init__(self, ax, X, pixdim, S):
-        self.ax = ax
-        ax.set_title('Atlas viewer')
-        print('use scroll wheel to navigate the atlas \n')
-        
-        self.X = X
-        rows, self.slices, cols = X.shape
-        self.ind = S
-        self.im = ax.imshow(self.X[:, self.ind, :], origin="lower", alpha=0.5, extent=[0, 512*pixdim, 512*pixdim, 0], cmap='gray')
-        self.update()
-
-    def onscroll(self, event):
-        #print("%s %s" % (event.button, event.step))
-        if event.button == 'up':
-            self.ind = (self.ind + 1) % self.slices
-        else:
-            self.ind = (self.ind - 1) % self.slices
-        self.update()        
-        
-    def update(self):
-        self.im.set_data(self.X[:, self.ind, :])
-        #ndimage.rotate(img, 45, reshape=False)
-        ax.set_ylabel('slice %d' % self.ind)
-        self.im.axes.figure.canvas.draw()      
-        
+# =============================================================================
+# class IndexTracker_cg(object):
+#     def __init__(self, ax, X, pixdim, S):
+#         self.ax = ax
+#         ax.set_title('Atlas viewer')
+#         print('use scroll wheel to navigate the atlas \n')
+#         
+#         self.X = X
+#         rows, self.slices, cols = X.shape
+#         self.ind = S
+#         self.im = ax.imshow(self.X[:, self.ind, :], origin="lower", alpha=0.5, extent=[0, 512*pixdim, 512*pixdim, 0], cmap='gray')
+#         self.update()
+# 
+#     def onscroll(self, event):
+#         #print("%s %s" % (event.button, event.step))
+#         if event.button == 'up':
+#             self.ind = (self.ind + 1) % self.slices
+#         else:
+#             self.ind = (self.ind - 1) % self.slices
+#         self.update()        
+#         
+#     def update(self):
+#         self.im.set_data(self.X[:, self.ind, :])
+#         #ndimage.rotate(img, 45, reshape=False)
+#         ax.set_ylabel('slice %d' % self.ind)
+#         self.im.axes.figure.canvas.draw()      
+#         
+# =============================================================================
         
         
 
@@ -202,6 +142,7 @@ file_name = input('Histology file name: ')
 #img_hist = cv2.imread(r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed/rat_processed.tif',cv2.IMREAD_GRAYSCALE)
 # Windows
 img_hist_temp = Image.open(os.path.join(processed_histology_folder, file_name+'_processed.jpeg')).copy()
+# get the pixel dimension
 dpi_hist = img_hist_temp.info['dpi'][1]
 pixdim_hist = 25.4/dpi_hist # 1 inch = 25,4 mm
 img_hist = cv2.imread(os.path.join(processed_histology_folder, file_name+'_processed.jpeg'),cv2.IMREAD_GRAYSCALE)
@@ -233,6 +174,7 @@ labels_index, labels_name, labels_color = readlabel( labels_item )
 # Windows
 atlas = nib.load(atlas_path)
 atlas_header = atlas.header
+# get pixel dimension
 pixdim = atlas_header.get('pixdim')[1]
 #atlas_data = atlas.get_fdata()
 #atlas_affine = atlas.affine
@@ -263,29 +205,26 @@ cv_plot = np.load('cv_plot.npy')/255
 # atlas_data[CC] = 0
 ##################
 
-# Display the atlas
+# Display the ATLAS
 # resolution
 dpi_atl = 25.4/pixdim
 # Bregma coordinates
 textstr = 'Bregma (mm): c = %.3f, h = %.3f, s = %.3f \nBregma (voxels): c = 653, h = 440, s = 246' %( 653*pixdim, 440*pixdim, 246*pixdim)
 # these are matplotlib.patch.Patch properties
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+# Figure
+fig, ax = plt.subplots(1, 1)#, figsize=(float(d1)/dpi_atl,float(d2)/dpi_atl), dpi=dpi_atl)
+# scroll cursor
+tracker = IndexTracker(ax, atlas_data, pixdim, plane)
+fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+# place a text box with bregma coordinates in bottom left in axes coords
+ax.text(0.03, 0.03, textstr, transform=ax.transAxes, fontsize=6 ,verticalalignment='bottom', bbox=props)
 if plane.lower() == 'c':
     # dimensions
     d1 = 512
     d2 = 512
     d3 = 1024
-    fig, ax = plt.subplots(1, 1)#, figsize=(float(d1)/dpi_atl,float(d2)/dpi_atl), dpi=dpi_atl)
-    tracker = IndexTracker_c(ax, atlas_data, pixdim)
-    #tracker = IndexTracker_c(ax, cv_plot)
-    #tracker_overlay = IndexTracker_c(ax, atlas_data)
-    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
-    #fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
-    # place a text box with bregma coordinates 
-    # in bottom left in axes coords
-    ax.text(0.03, 0.03, textstr, transform=ax.transAxes, fontsize=6 ,verticalalignment='bottom', bbox=props)
-    # display the coordinates relative to the bregma
-    # when hovering with the cursor
+    # display the coordinates relative to the bregma when hovering with the cursor
     def format_coord(x, y):
         AP = tracker.ind*pixdim - 653*pixdim
         ML = y - 246*pixdim
@@ -295,7 +234,6 @@ if plane.lower() == 'c':
         else:
             return 'AP=%1.4f, ML=L%1.4f, z=%1.4f'%(AP, ML, Z)    
     ax.format_coord = format_coord
-    plt.show()
 # =============================================================================
 #     cursor = mplcursors.cursor(hover=True)
 #     # Show the names of the regions
@@ -314,15 +252,7 @@ elif plane.lower() == 's':
     d1 = 1024 
     d2 = 512    
     d3 = 512
-    fig, ax = plt.subplots(1, 1, figsize=(float(d1)/dpi_atl,float(d2)/dpi_atl))
-    tracker = IndexTracker_s(ax, atlas_data, pixdim)
-    #tracker = IndexTracker_s(ax, cv_plot)
-    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
-    # place a text box with bregma coordinates 
-    # in bottom left in axes coords
-    ax.text(0.03, 0.03, textstr, transform=ax.transAxes, fontsize=6 ,verticalalignment='bottom', bbox=props)
-    # display the coordinates relative to the bregma
-    # when hovering with the cursor
+    # display the coordinates relative to the bregma when hovering with the cursor
     def format_coord(x, y):
         AP = y - 653*pixdim
         ML = tracker.ind*pixdim - 246*pixdim
@@ -332,7 +262,6 @@ elif plane.lower() == 's':
         else:
             return 'AP=%1.4f, ML=L%1.4f, z=%1.4f'%(AP, ML, Z)    
     ax.format_coord = format_coord
-    plt.show()
 # =============================================================================
 #     cursor = mplcursors.cursor(hover=True)
 #     # Show the names of the regions 
@@ -351,15 +280,7 @@ elif plane.lower() == 'h':
     d1 = 512
     d2 = 1024
     d3 = 512    
-    fig, ax = plt.subplots(1, 1, figsize=(float(d1)/dpi_atl,float(d2)/dpi_atl))
-    tracker = IndexTracker_h(ax, atlas_data, pixdim)
-    #tracker = IndexTracker_h(ax, cv_plot)
-    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
-    # place a text box with bregma coordinates 
-    # in bottom left in axes coords
-    ax.text(0.03, 0.03, textstr, transform=ax.transAxes, fontsize=6 ,verticalalignment='bottom', bbox=props)
-    # display the coordinates relative to the bregma
-    # when hovering with the cursor
+    # display the coordinates relative to the bregma when hovering with the cursor
     def format_coord(x, y):
         AP = x - 653*pixdim
         ML = y - 246*pixdim        
@@ -369,7 +290,7 @@ elif plane.lower() == 'h':
         else:
             return 'AP=%1.4f, ML=L%1.4f, z=%1.4f'%(AP, ML, Z)    
     ax.format_coord = format_coord
-    plt.show()
+plt.show()    
 # =============================================================================
 #     cursor = mplcursors.cursor(hover=True)
 #     # Show the names of the regions
@@ -383,6 +304,7 @@ elif plane.lower() == 'h':
 #         sel.annotation.set_text(Text)
 #     cursor.connect('add', show_annotation)
 # =============================================================================
+
 # Fix size and location of the figure window
 mngr = plt.get_current_fig_manager()
 mngr.window.setGeometry(800,300,d2,d1)     
@@ -390,15 +312,13 @@ mngr.window.setGeometry(800,300,d2,d1)
 # Store the current slice
 S = tracker.ind 
 
-
-# Show the processed histology
+# Show the HISTOLOGY
 # Set up figure
 fig_hist, ax_hist = plt.subplots(1, 1, figsize=(float(img_hist.shape[1])/dpi_hist,float(img_hist.shape[0])/dpi_hist))
-# Remove whitespace from around the image
-#fig_hist.subplots_adjust(left=0,right=1,bottom=0,top=1)
 ax_hist.set_title("Histology viewer")
 # Show the histology image  
 ax_hist.imshow(img_hist_temp, extent=[0, img_hist.shape[1]*pixdim_hist, img_hist.shape[0]*pixdim_hist, 0])
+# Remove axes tick
 plt.tick_params(
     axis='both', 
     which='both',      # both major and minor ticks are affected
@@ -407,6 +327,7 @@ plt.tick_params(
     top=False,         # ticks along the top edge are off
     labelbottom=False,
     labelleft=False) # labels along the bottom edge are off
+# Remove cursor position
 ax_hist.format_coord = lambda x, y: ''
 plt.show()
 # Fix size and location of the figure window
@@ -428,18 +349,18 @@ coords_hist = []
 redp_atlas = []
 redp_hist = []
 
-Edges = np.empty((512,1024,512))
-for sl in range(0,1024):
-    Edges[:,sl,:] = cv2.Canny(np.uint8((cv_plot[:,sl,:]*255).transpose((1,0,2))),100,200)  
+# =============================================================================
+# Edges = np.empty((512,1024,512))
+# for sl in range(0,1024):
+#     Edges[:,sl,:] = cv2.Canny(np.uint8((cv_plot[:,sl,:]*255).transpose((1,0,2))),100,200)  
+# =============================================================================
 
 # Reaction to key pressed
 def on_key(event):
     if event.key == 't':
         print('Select at least 4 points in the same order in both figures')
         # ATLAS
-        # Mouse click function to store coordinates.
-        # Leave a red dot when a point is clicked
-        # when registering slides        
+        # Mouse click function to store coordinates. Leave a red dot when a point is clicked
         def onclick(event):
             global ix, iy
             ix, iy = event.xdata/pixdim, event.ydata/pixdim
@@ -453,9 +374,7 @@ def on_key(event):
         fig.canvas.mpl_connect('button_press_event', onclick)  
   
         # HISTOLOGY  
-        # Mouse click function to store coordinates.
-        # Leave a red dot when a point is clicked
-        # when registering slides        
+        # Mouse click function to store coordinates. Leave a red dot when a point is clicked      
         def onclick_hist(event):
             global xh, yh
             xh, yh = event.xdata/pixdim_hist, event.ydata/pixdim_hist
@@ -505,17 +424,12 @@ def on_key(event):
             edges = cv2.Canny(np.uint8((cv_plot[S,:,:]*255).transpose((1,0,2))),100,200)
         elif plane.lower() == 'h':    
             edges = cv2.Canny(np.uint8((cv_plot[:,:,S]*255).transpose((1,0,2))),100,200)
-            
+        # Set up the figure    
         fig_grid, ax_grid = plt.subplots(1, 1) 
+        # position of the lines
         CC = np.where(edges == 255)
-# =============================================================================
-#         GRAY SCALE
-#         img = cv2.merge((img_warped,img_warped,img_warped))  # creat RGB image from grayscale
-#         img2 = (img).copy()
-#         img2[CC] = [0.25,0.41,0.88]  # turn edges to blue
-#         ax_grid.imshow(img2, origin="lower")
-# =============================================================================
         img2 = (img_warped).copy()
+        # get the lines in the warped figure
         img2[CC] = 0.5
         ax_grid.imshow(img2, origin="lower", extent=[0, d1*pixdim, 0, d2*pixdim])   
         ax_grid.text(0.15, 0.05, textstr, transform=ax.transAxes, fontsize=6 ,verticalalignment='bottom', bbox=props)
