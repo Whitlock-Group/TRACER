9c6a1987ba24c55a756404c638a6d167afbe10fb
JacopoPaglia <61981085+JacopoPaglia@users.noreply.github.com>
2021-01-04 19:46:32 +0100

    Add files via upload
    new tracking options for finding insertions angle

--- Tracker.py
+++ Tracker.py
@@ -125,3 +125,90 @@ class IndexTracker_p(object):
             self.im.set_data(self.X[:, :, self.ind].T)
         self.ax.set_ylabel('slice %d' % self.ind)
         self.im.axes.figure.canvas.draw()        
+        
+        
+# Class to scroll the boundaries atlas slices (coronal, sagittal and horizontal)            
+class IndexTracker_b(object):
+    def __init__(self, ax, X, pixdim, p, S):
+        self.ax = ax
+        self.plane = p.lower()
+        ax.set_title('Atlas viewer')
+        print('use scroll wheel to navigate the atlas \n')
+        
+        self.X = X
+        if self.plane == 'c':
+            rows, self.slices, cols = X.shape
+            self.ind = S
+            self.im = ax.imshow(self.X[:, self.ind, :], origin="lower", alpha=0.5, extent=[0, 512*pixdim, 0, 512*pixdim,], cmap='gray')
+        elif self.plane == 's':
+            self.slices, rows, cols = X.shape
+            self.ind = S                
+            self.im = ax.imshow(self.X[self.ind, :, :].T, origin="lower", extent=[0 ,1024*pixdim, 512*pixdim , 0], cmap='gray')            
+        elif self.plane == 'h':  
+            rows, cols, self.slices = X.shape
+            self.ind = S       
+            self.im = ax.imshow(self.X[:, :, self.ind].T, origin="lower", extent=[0, 512*pixdim, 1024*pixdim, 0], cmap='gray')  
+        self.update()
+
+    def onscroll(self, event):
+        if event.button == 'up':
+            self.ind = (self.ind + 1) % self.slices
+        else:
+            self.ind = (self.ind - 1) % self.slices
+        self.update()        
+        
+    def update(self):
+        if self.plane == 'c':
+            self.im.set_data(self.X[:, self.ind, :])
+        elif self.plane == 's':
+            self.im.set_data(self.X[self.ind, :, :])
+        elif self.plane == 'h':
+            self.im.set_data(self.X[:, :, self.ind])
+        self.ax.set_ylabel('slice %d' % self.ind)
+        self.im.axes.figure.canvas.draw()           
+        
+
+# Class to scroll the color atlas slices (coronal, sagittal and horizontal)            
+class IndexTracker_c(object):
+    def __init__(self, ax, X, pixdim, p, S):
+        self.ax = ax
+        self.plane = p.lower()
+        ax.set_title('Atlas viewer')
+        print('use scroll wheel to navigate the atlas \n')
+        
+        self.X = X
+        if self.plane == 'c':
+            rows, self.slices, cols, color = X.shape
+            self.ind = S
+            self.im = ax.imshow(self.X[:, self.ind, :], origin="lower", alpha=0.5, extent=[0, 512*pixdim, 0, 512*pixdim], cmap='gray')
+        elif self.plane == 's':
+            self.slices, rows, cols, color= X.shape
+            self.ind = S                
+            self.im = ax.imshow(self.X[self.ind, :, :].T, origin="lower", extent=[0 ,1024*pixdim, 512*pixdim , 0], cmap='gray')            
+        elif self.plane == 'h':  
+            rows, cols, self.slices, color = X.shape
+            self.ind = S       
+            self.im = ax.imshow(self.X[:, :, self.ind].T, origin="lower", extent=[0, 512*pixdim, 1024*pixdim, 0], cmap='gray')  
+        self.update()
+
+    def onscroll(self, event):
+        if event.button == 'up':
+            self.ind = (self.ind + 1) % self.slices
+        else:
+            self.ind = (self.ind - 1) % self.slices
+        self.update()        
+        
+    def update(self):
+        if self.plane == 'c':
+            self.im.set_data(self.X[:, self.ind, :])
+        elif self.plane == 's':
+            self.im.set_data(self.X[self.ind, :, :])
+        elif self.plane == 'h':
+            self.im.set_data(self.X[:, :, self.ind])
+        self.ax.set_ylabel('slice %d' % self.ind)
+        self.im.axes.figure.canvas.draw()                
+                 
+        
+        
+        
+        
