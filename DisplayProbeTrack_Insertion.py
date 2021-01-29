b2fceb854fba2c55228f69bcf001a6161421c77f
JacopoPaglia <61981085+JacopoPaglia@users.noreply.github.com>
2021-01-29 10:43:50 +0100

    Add files via upload

--- DisplayProbeTrack_Insertion.py
+++ DisplayProbeTrack_Insertion.py
@@ -1,6 +1,10 @@
+
+
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jan 19 11:33:32 2021
+Created on Fri Jan 15 10:13:07 2021
+
 @author: jacopop
 """
 from __future__ import print_function
@@ -23,8 +27,8 @@ import pickle
 from skimage import measure
 from collections import OrderedDict, Counter
 from tabulate import tabulate
-from scipy.spatial import distance
-    
+from scipy.spatial import distance    
+
 # 3d Brain
 from vedo import Volume as VedoVolume
 import vedo 
@@ -51,7 +55,6 @@ electrode = 0.012 # Electrode size is 12x12 micron
 vert_el_dist = 0.02 
 # There are 2 electrodes every 0.02 mm
 
-
 # Mac
 atlas = nib.load(r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/Waxholm_Atlas/WHS_SD_rat_atlas_v2_pack/WHS_SD_rat_T2star_v1.01.nii.gz')
 # Windows
@@ -66,56 +69,40 @@ pixdim = atlas_header.get('pixdim')[1]
 # Labels
 # Mac
 labels_item = open(r"/Users/jacopop/Box Sync/macbook/Documents/KAVLI/Waxholm_Atlas/WHS_SD_rat_atlas_v4_beta.label", "r")
-# =============================================================================
-# # Windows
+# Windows
 # labels_item = open(r"C:\Users\jacopop\Box Sync\macbook\Documents\KAVLI\Waxholm_Atlas\WHS_SD_rat_atlas_v4_beta.label", "r")
-# =============================================================================
 labels_index, labels_name, labels_color, labels_initial = readlabel( labels_item )   
 
 # Segmentation
 # Mac
 segmentation = nib.load('/Users/jacopop/Box Sync/macbook/Documents/KAVLI/Waxholm_Atlas/WHS_SD_rat_atlas_v4_beta.nii.gz')
 # Windows
-# =============================================================================
-# segmentation_path = os.path.join(r'C:\Users\jacopop\Box Sync\macbook\Documents\KAVLI\Waxholm_Atlas', 'WHS_SD_rat_atlas_v4_beta.nii.gz')
-# segmentation = nib.load(segmentation_path)
-# =============================================================================
+#segmentation = nib.load(segmentation_path)
 segmentation_data = segmentation.get_fdata()
 
 # Probe colors
 probe_colors = ['purple', 'blue', 'yellow', 'orange', 'red', 'green']
 
-# =============================================================================
-# # Windows
-# processed_histology_folder = r'C:\Users\jacopop\Box Sync\macbook\Documents\KAVLI\histology\processed'
-# path_probes = os.path.join(processed_histology_folder, 'probes')
-# path_transformed = os.path.join(processed_histology_folder, 'transformations')
-# =============================================================================
 
-# Mac
-processed_histology_folder = r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed'
-path_probes = r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed/probes'
-path_transformed = '/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed/transformations'
+
+path_probe_insertion = '/Users/jacopop/Box Sync/macbook/Documents/KAVLI/Probe_Insertion'
 
 # get the all the files in the probe folder
-files_probe = os.listdir(path_probes)
-files_transformed = os.listdir(path_transformed)
+files_probe = os.listdir(path_probe_insertion)
 
 L = probe_obj()
 LINE_FIT = probe_obj() 
 pr = probe_obj()
 xyz = probe_obj()
-LENGTH = probe_obj()
 P = []
 color_used_t = []
-for f in files_probe:
-    # WINDOWS
-    P.append(pickle.load(open(os.path.join(path_probes, f), "rb")))
 # =============================================================================
-#     # MAC
-# P.append(pickle.load(open(r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed/probes/1probes.pkl', "rb")))
-# P.append(pickle.load(open(r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/histology/processed/probes/2probes.pkl', "rb")))
-# =============================================================================
+# for f in files_probe:
+    #     # WINDOWS
+    #     P.append(pickle.load(open(os.path.join(path_probes, f), "rb")))
+    # =============================================================================
+    # MAC
+P.append(pickle.load(open(r'/Users/jacopop/Box Sync/macbook/Documents/KAVLI/Probe_Insertion/0probes.pkl', "rb")))
 # LL = pickle.load(open(os.path.join(path_probes, '1probes.pkl'), "rb"))    
 probe_counter = P[0].Counter
 
@@ -131,21 +118,22 @@ for j in range(len(probe_colors)):
             if P[k].Plane == 'c':
                 for i in range(len(PC)):
                     probe_x.append(PC[i][0])
-                    probe_y.append(P[k].Slice)
+                    probe_y.append(PC[i][2])
                     probe_z.append(PC[i][1])
-            elif P[k].Plane == 's':
+            elif P[k].Plane == 'c':
                 for i in range(len(PC)):
-                    probe_x.append(P[k].Slice)
+                    probe_x.append(PC[i][2])
                     probe_y.append(PC[i][0])
                     probe_z.append(PC[i][1])  
-            elif P[k].Plane == 'h':
+            elif P[k].Plane == 'c':
                 for i in range(len(PC)):        
                     probe_x.append(PC[i][0])
                     probe_y.append(PC[i][1])        
-                    probe_z.append(P[k].Slice)
+                    probe_z.append(PC[i][2])
             pts = np.array((probe_x, probe_y, probe_z)).T
+
             # fit the probe
-            line_fit = Line.best_fit(pts)
+            line_fit = Line.best_fit(pts)            
             # if no inclination in z direction
             if line_fit.direction[2] == 0:
                 # line equations, to derive the starting and end point of the line (aka probe)
@@ -155,7 +143,7 @@ for j in range(len(probe_colors)):
                 y1 = line_fit.point[1]+((x1-line_fit.point[0])/line_fit.direction[0])*line_fit.direction[1]
                 z2 = pts[0,2]
                 x2 = x2 = pts[-1,0]
-                y2 = line_fit.point[1]+((x2-line_fit.point[0])/line_fit.direction[0])*line_fit.direction[1]             
+                y2 = line_fit.point[1]+((x2-line_fit.point[0])/line_fit.direction[0])*line_fit.direction[1]               
             else:
                 # line equations, to derive the starting and end point of the line (aka probe)
                 # it is impartant that the first point clicked is the most external one, and the last correspond to the end of the probe
@@ -165,14 +153,12 @@ for j in range(len(probe_colors)):
                 z2 = pts[-1,2]
                 x2 = line_fit.point[0]+((z2-line_fit.point[2])/line_fit.direction[2])*line_fit.direction[0]
                 y2 = line_fit.point[1]+((z2-line_fit.point[2])/line_fit.direction[2])*line_fit.direction[1]
-            dist = distance.euclidean((x1, y1, z1), (x2, y2, z2)) # probe length
-            dist_mm = dist*pixdim # probe length in mm              
-            # get the line to plot                
+                # get the line to plot                
+            # get the line to plot
             l = vedo.Line([x1, y1, z1],[x2, y2, z2],c = probe_colors[j], lw = 2)
             # clicked points to display
             pp = vedo.Points(pts, c = probe_colors[j]) #fast    
-            setattr(xyz, probe_colors[j], [[x1, y1, z1], [x2, y2, z2]])
-            setattr(LENGTH, probe_colors[j], [dist_mm, dist])
+            setattr(xyz,probe_colors[j], [[x1, y1, z1], [x2, y2, z2]])
             setattr(pr, probe_colors[j], pp)
             setattr(L, probe_colors[j], l)
             setattr(LINE_FIT, probe_colors[j], line_fit)
@@ -193,11 +179,11 @@ mask_data = mask.get_fdata()[:,:,:,0].transpose((2,1,0))
 Edges = np.empty((512,1024,512))
 for sl in range(0,1024):
     Edges[:,sl,:] = cv2.Canny(np.uint8((mask_data[:,sl,:])*255),100,200)  
-
 edges = Edges.T
-edges[:,::2,:] = edges[:,::2,:]*0
+
+edges[:,::2,::] = edges[:,::2,::]*0
 edges[:,::5,:] = edges[:,::5,:]*0
-edges[::2,:,:] = edges[::2,:,:]*0
+edges[::2,:,::] = edges[::2,:,::]*0
 edges[:,:,::2] = edges[:,:,::2]*0
 
 coords = np.array(np.where(edges == 255))
@@ -219,7 +205,10 @@ lut = buildLUT([
               )
 mesh.cmap(lut, data)
 
+dist = []
+
 # To plot the probe with colors
+
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111, aspect='equal')
 # compute and display the insertion angle for each probe
@@ -227,7 +216,6 @@ for i in range(0,n):
     line_fit = getattr(LINE_FIT, color_used[i])
     deg_lat = math.degrees(math.atan(line_fit.direction[0]))
     deg_ant = math.degrees(math.atan(line_fit.direction[1]))
-    Length = getattr(LENGTH, color_used[i])
     print('\n\nAnalyze %s probe: \n ' %color_used[i])
     print('Estimated %s probe insertion angle: ' %color_used[i])
     print('%.2f degrees in the anterior direction' %deg_ant)
@@ -236,52 +224,49 @@ for i in range(0,n):
     # Get the brain regions traversed by the probe
     X1 = getattr(xyz, color_used[i])[0]
     X2 = getattr(xyz, color_used[i])[1]
-
+    s = int(math.modf(X1[2])[1]) # starting point
+    f = int(math.modf(X2[2])[1]) # ending point
+    # get lenght of the probe
+    dist.append(np.linalg.norm(f-s))
     regions = []
     colori = []
     initials = []
     index = []
-    channels = []
     point_along_line = []
     if line_fit.direction[2] == 0:                
-        s = int(math.modf(X1[0])[1]) # starting point
-        f = int(math.modf(X2[0])[1]) # ending point
         for x in range(min(s,f), max(s,f)):                        
             y = line_fit.point[1]+((x-line_fit.point[0])/line_fit.direction[0])*line_fit.direction[1]
             z = pts[0,2]
-            point_along_line.append([x,y,z])
-            regions.append(labels_name[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
-            colori.append(labels_color[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
-            initials.append(labels_initial[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
-            #index.append(segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])])
-            #channels.append(0)    
-    else:    
-        s = int(math.modf(X1[2])[1]) # starting point
-        f = int(math.modf(X2[2])[1]) # ending point
+            point_along_line.append([x,y,z])            
+            if int(math.modf(x)[1])>512 or int(math.modf(y)[1])>1024 or int(math.modf(z)[1])>512:
+                regions.append('Clear Label')
+                colori.append('black')
+                initials.append('CL')
+            else:            
+                regions.append(labels_name[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+                colori.append(labels_color[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+                initials.append(labels_initial[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+    else:
         for z in range(min(s,f),max(s,f)):
             x = line_fit.point[0]+((z-line_fit.point[2])/line_fit.direction[2])*line_fit.direction[0]
             y = line_fit.point[1]+((z-line_fit.point[2])/line_fit.direction[2])*line_fit.direction[1]
             point_along_line.append([x,y,z])
-            regions.append(labels_name[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
-            colori.append(labels_color[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
-            initials.append(labels_initial[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+            if int(math.modf(x)[1])>512 or int(math.modf(y)[1])>1024 or int(math.modf(z)[1])>512:
+                regions.append('Clear Label')
+                colori.append('black')
+                initials.append('CL')
+            else:            
+                regions.append(labels_name[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+                colori.append(labels_color[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
+                initials.append(labels_initial[np.argwhere(np.all(labels_index == segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])], axis = 1))[0,0]])
             #index.append(segmentation_data[int(math.modf(x)[1]),int(math.modf(y)[1]),int(math.modf(z)[1])])
-            #channels.append(0)       
-    # get lenght of the probe
-    if Length[0] > max_probe_length+probe_tip_length:
-        print('ERROR: probe %d (%s) exceed the maximum probe length (10mm)!\n' %(i+1, color_used[i]))    
-    recording_probe = Length[0]-probe_tip_length  # subtract the tip of the probe to consider only the part with electrodes
-    electrodes_inside = round((recording_probe/vert_el_dist)*2)# 2 electrodes avery 20 micron
-            
-    # count the number of elements in each region to             
+    # count the number of elements in each region to 
     counter_regions = dict(Counter(regions))    
     regioni = list(OrderedDict.fromkeys(regions))
     iniziali = list(OrderedDict.fromkeys(initials))
-    # remove clear label
     if 'Clear Label' in regioni:
-        indice = regioni.index('Clear Label')
-        regioni.pop(indice)
-        iniziali.pop(indice)
+        regioni.pop(regioni.index('Clear Label'))
+        iniziali.pop(regioni.index('Clear Label'))
     cc = 0
     jj = 0
     num_el = []
@@ -299,31 +284,31 @@ for i in range(0,n):
             # length of the part of the probe in the region
             regional_dist = distance.euclidean(start,end)  
         # Number of electrodes in the region                
-        num_el.append(round(regional_dist/vert_el_dist)*2)               
+        num_el.append(round(regional_dist/vert_el_dist)*2)                                 
         #print(re)
         # proportion of the probe in the given region
-        dist_prop = counter_regions[re]/Length[1]
+        dist_prop = counter_regions[re]/dist[i]
         color_prop = labels_color[np.argwhere(np.array(labels_name)== re)]
-        # plot the probe with the colors of the region traversed
-        ax1.add_patch(patches.Rectangle((70*i+20, cc), 15, dist_prop*Length[1]*1.2, color=color_prop[0][0]/255))
-        plt.text(70*i+20, max_probe_length/pixdim+2, 'Probe %d\n(%s)'%(i+1, color_used[i]), color = color_used[i], fontsize=7.5, fontweight='bold')
-        plt.text(70*i-3, cc+round(dist_prop*Length[1]*1.2/2), '%s'%(iniziali[jj]), fontsize=5.5)
-        plt.text(70*i+41, cc+round(dist_prop*Length[1]*1.2/2), '%d'%(num_el[jj]), fontsize=5.5)
+        # plot the probe with the colors of the region traversed       
+        ax1.add_patch(patches.Rectangle((70*i+20, cc), 15, dist_prop*dist[i], color=color_prop[0][0]/255))
+        plt.text(70*i+20, max(dist)+2, 'Probe %d\n(%s)'%(i+1, color_used[i]), color = color_used[i], fontsize=7.5, fontweight='bold')
+        plt.text(70*i-5, cc+round(dist_prop*dist[i]/2), '%s'%(iniziali[jj]), fontsize=5.5)
+        plt.text(70*i+41, cc+round(dist_prop*dist[i]/2), '%d'%(num_el[jj]), fontsize=5.5)
         jj +=1
-        cc = dist_prop*Length[1] + cc    
-        del regional_dist, position 
-        
-    #indici = list(OrderedDict.fromkeys(index))    
+        cc = dist_prop*dist[i] + cc    
+    #indici = list(OrderedDict.fromkeys(index))
     LL = [regioni,  iniziali, num_el]
     headers = [' Regions traversed', 'Initials', 'Channels']
     numpy_array = np.array(LL)
     transpose = numpy_array.T
     transpose_list = transpose.tolist()
     print(tabulate(transpose_list, headers, floatfmt=".2f"))
-lims = (0,max_probe_length/pixdim)
+lims = (0,max(dist))
 plt.ylim(lims)
 plt.xlim((0,70*n+20))
 plt.axis('off')
+
+
 
 # plot all the probes together
 if n==1:
@@ -336,7 +321,8 @@ elif n==2:
      )
 elif n == 3:
      show(mesh, getattr(pr,color_used[0]), getattr(pr,color_used[1]), getattr(pr,color_used[2]), getattr(L, color_used[0]),getattr(L, color_used[1]), getattr(L, color_used[2]), __doc__,
-     axes=0, viewup="z", bg='black', 
+     axes=0, viewup="z",
+     bg='black', 
      )
 elif n == 4:
      show(mesh, getattr(pr,color_used[0]), getattr(pr,color_used[1]), getattr(pr,color_used[2]), getattr(pr,color_used[3]), getattr(L, color_used[0]), getattr(L, color_used[1]), getattr(L, color_used[2]), getattr(L, color_used[3]), __doc__,
@@ -350,14 +336,6 @@ elif n == 6:
      show(mesh, getattr(pr,color_used[0]), getattr(pr,color_used[1]), getattr(pr,color_used[2]), getattr(pr,color_used[3]), getattr(pr,color_used[4]), getattr(pr,color_used[5]), getattr(L, color_used[0]), getattr(L, color_used[1]), getattr(L, color_used[2]), getattr(L, color_used[3]), getattr(L, color_used[4]), getattr(L, color_used[5]), __doc__,
      axes=0, viewup="z", bg='black',
      )
-
-
-
-
-
-
-
-
 
 
 
